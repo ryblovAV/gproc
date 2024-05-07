98e83377833aa6a0a3194a7214df72eb60b2dd9b
AlexanderRyblov <a.ryblov@efg.gg>
2024-05-07 23:49:16 +0300

    debug

--- src/gproc_pool.erl
+++ src/gproc_pool.erl
@@ -631,8 +631,8 @@ call(Req) ->
     
     try gen_server:call(?MODULE, Req)
     catch 
-        Error:Reason:Stacktrace ->
-            ?LOG_DEBUG(#{what => debug_gproc_pool_error, error => Error, reason => Reason, stacktrace => Stacktrace}),
+        E:R:S ->
+            ?LOG_DEBUG(#{what => debug_gproc_pool_error, error => E, reason => R, stacktrace => S})
     end,        
     
     case gen_server:call(?MODULE, Req) of
